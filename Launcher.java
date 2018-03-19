75681ed7367aefe9ebe77b4bd975cc461f1f3c53
Ramses Kamanda <ramseskamanda@georgettes-air.wireless.unimaas.local>
2018-03-19 13:13:44 +0100

    Reorganized everything

--- Launcher.java
+++
@@ -1,7 +0,0 @@
-public class Launcher {
-    public static void main(String[] args){
-        Game game= new Game("Golf Game",80,80);
-        game.start();
-
-    }
-}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Project2.1
+
+Commit changes through git bash please
+DO NOT REUPLOAD EVERYTIME
+CREATE NEW BRANCHES FOR MAJOR CHANGES
