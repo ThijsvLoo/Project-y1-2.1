82ca0c3fcdb31bbcb3e7413e6a8a7bdc7ca8460a
Ramses Kamanda <ramseskamanda@georgettes-air.home>
2018-03-21 20:44:24 +0100

    Just another commit

--- src/Player.java
+++ src/Player.java
@@ -4,11 +4,13 @@ import java.awt.image.BufferedImage;
 public class Player extends Entity {
   private static BufferedImage[] sprite;
   private static int iterator;
+  private static Physics engine;
 
   public Player(BufferedImage[] image, float x, float y, int width, int height) {
     super(x, y, width, height);
     this.sprite = image;
     this.iterator = 0;
+    //this.engine = new Physics();
   }
 
   public void render(Graphics g){
