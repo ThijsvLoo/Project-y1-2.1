27eeb4a0b7a4f5fbb3a507804792f80f6eb7db81
Babis <mpampis.laz@gmail.com>
2018-03-21 11:19:15 +0100

    Added comments to the ball motion method

--- src/Physics.java
+++ src/Physics.java
@@ -20,28 +20,44 @@ public class Physics {
 
 	public void ballMotion(double frictionC, double slopeAngle, double slopeDirection) {
 
+    /* The velocity array contains the Velocity of the x and y axis respectively */
 		double[] velocityAr = {ballVelocity*Math.cos(velocityAngle), ballVelocity*Math.sin(velocityAngle)};
+
+      /* The acceleration array contains the Velocity of the x and y axis respectively */
 		double[] accelerationAr = new double[2];
 
+    /* The gravity Force */
 		double gravity = ballMass*grAcceleration ;
+
+    /* The effective gravity force, parralel to the motion */
 		double parallelGr = gravity*Math.sin(slopeAngle);
+
+    /* The gravity force vertical to the motion (needed to calculate the friction)*/
 		double verticalGr = gravity*Math.cos(slopeAngle);
+
+    /* The gravityAr contains the effective gravity for the x and y axis respectively */
 		double[] gravityAr = {parallelGr*Math.cos(slopeDirection), parallelGr*Math.sin(slopeDirection)};
 
+    /* The friction force */
 		double friction=frictionC*verticalGr;
+
+    /* The frictionAr contains the friction for the x and y axis respectively */
 		double[] frictionAr = {friction*Math.cos(Math.PI+velocityAngle), friction*Math.sin(Math.PI+velocityAngle)};
+
+    /* The time step */
 		double delta = 1.0;
 
 
-		gravity=ballMass*grAcceleration;
 
-
+      /* Equations of Acceleration*/
 		accelerationAr[0]=(frictionAr[0]+gravityAr[0])/ballMass;
 		accelerationAr[1]=(frictionAr[1]+gravityAr[1])/ballMass;
 
+          /* The equations of Velocity */
 		velocityAr[0] += accelerationAr[0]*delta;
 		velocityAr[1] += accelerationAr[1]*delta;
 
+            /* The equations of the position */
 		this.ballPosition[0] += velocityAr[0]*delta + 0.5*accelerationAr[0]*delta*delta;
 		this.ballPosition[1] += velocityAr[1]*delta + 0.5*accelerationAr[1]*delta*delta;
 
