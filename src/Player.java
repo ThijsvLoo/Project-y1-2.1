53a5e0f1b09704308c56706f2ce14b791b63b43a
Ramses Kamanda <ramseskamanda@georgettes-air.wireless.unimaas.local>
2018-03-21 12:04:08 +0100

    Player class added

--- src/Player.java
+++ src/Player.java
@@ -1,41 +1,28 @@
-
-
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 
 public class Player extends Entity {
+  private static BufferedImage[] sprite;
+  private static int iterator;
 
-  private int width;
-  private int height;
-  private double boundsRadius;
-  private float boundsX;
-  private float boundsY;
+  public Player(float x, float y, int width, int height) {
+    super(x, y, width, height);
+    this.sprite = Assets.ball;
+    this.iterator = 0;
+  }
 
+  public void render(Graphics g){
+    g.drawImage(this.sprite[this.iterator], (int) this.x, (int) this.y, this.width, this.height, null);
+    tick();
+  }
 
+  public void tick() {     //This will update everything
+    this.iterator++;
+    if(this.iterator >= 2) this.iterator = 0;
+  }
+
+  public void move() {
+    this.x++;
+    this.y++;
+  }
 }
-
-public Player() {
-  this.width = width;
-  this.height = height;
-  this.boundsRadius = boundsRadius;
-  this.boundsX = boundsX;
-  this.boundsY = boundsY
-
-}
-
-public void render(Graphics.j){
-  //TODO
-
-}
-
-public void tick() {
-  //TODO
-  //This will update everything
-}
-
-public move() {
-  //TODO
-}
-
-public 
-
-
-
