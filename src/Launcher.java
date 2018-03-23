9d818e63b9059c8e518bc367f552ebcd355be014
Thieskeloo <thijsvanloo4@gmail.com>
2018-03-23 12:25:05 +0100

    added music

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,9 +1,22 @@
 import java.awt.*;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.*;
 
 public class Launcher {
     public static void main(String[] args){
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         Game game= new Game("Golf Game",screenSize.width,screenSize.height);
+
+        try{
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("resources/music.wav"));
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.loop(999);
+        }catch(Exception ex){ }
+
+
         game.start();
 
     }
